cb9fd0b09c511f81e9f6e4e248bbb4cef4d3eb21
Harsh Pandey <coding.harshp@gmail.com>
2022-08-10 11:33:37 +0530

    Secret Api

--- components/BurnToken.jsx
+++ components/BurnToken.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "@/styles/BurnToken.module.css";
 
 export default function BurnToken() {
+  const [message, setMessage] = useState("");
+
+  const handleBurn = async () => {
+    setMessage("");
+
+    const res = await fetch("http://localhost:3000/api/secret");
+    const data = await res.json();
+
+    setMessage(data.message);
+  };
+
   return (
     <div className={styles.container}>
       <h2 id="heading">Secret Message</h2>
-      <div className={styles.content}>
-        <p>
-          Click on the area to <span id={styles.action}>Burn 1 HKP Token</span>{" "}
-          to see the message
-        </p>
-      </div>
+      {!message && (
+        <div onClick={handleBurn} className={styles.content}>
+          <p>
+            Click on the area to{" "}
+            <span id={styles.action}>Burn 1 HKP Token</span> to see the message
+          </p>
+        </div>
+      )}
+      {message && (
+        <div className={styles.message}>
+          <p>“{message}”</p>
+          <button onClick={handleBurn} id={styles.burn}>
+            Burn more
+          </button>
+        </div>
+      )}
     </div>
   );
 }
