670f4fe63dae9fe112a7d9980b5b62c27cf82cab
Harsh Pandey <coding.harshp@gmail.com>
2022-08-11 03:07:42 +0530

    Events Model

--- components/Transactions.jsx
+++ components/Transactions.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "@/styles/Transactions.module.css";
 import { GoLinkExternal } from "react-icons/go";
+import { ethers } from "ethers";
+import { useWalletContext } from "context/walletContext";
+import { eventColor, formatAddress } from "@/helpers/constants";
+import moment from "moment";
 
 export default function Transactions() {
+  const { events } = useWalletContext();
+
   return (
     <div className={styles.container}>
       <h2 id="heading">Transaction Logs</h2>
       <div className={styles.content}>
-        <Transaction />
-        <Transaction />
-        <Transaction />
-        <Transaction />
+        {events.map((event) => (
+          <Transaction key={event._id} event={event} />
+        ))}
       </div>
       <a href="#" className={styles.contract}>
         View Smart Contract on Etherscan
@@ -20,13 +25,21 @@ export default function Transactions() {
   );
 }
 
-function Transaction() {
+function Transaction({ event }) {
   return (
     <div className={styles.block}>
       <p className={styles.action}>
-        0xDb23....2D5A <span className={styles.method}>Minted</span> 2 HKP
-        Tokens from Smart Contract{" "}
-        <span className={styles.timestamp}>36 mins ago</span>
+        {formatAddress(event.from)}{" "}
+        <span
+          style={{ color: eventColor[event.method] }}
+          className={styles.method}
+        >
+          {event.method}ed
+        </span>{" "}
+        2 HKP Tokens from Smart Contract{" "}
+        <span className={styles.timestamp}>
+          {moment(event.createdAt).startOf("hour").fromNow()}
+        </span>
       </p>
       <a href="#" className={styles.link}>
         View on Block Explorer
